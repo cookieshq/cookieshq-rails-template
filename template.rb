04fed1a23a696473009f39a3f81181f18463dad1
Julio Antequera Galiano <julio.antequera.galiano@gmail.com>
2014-10-09 16:34:54 +0200

    Changes acceptance test

--- template.rb
+++ template.rb
@@ -383,7 +383,7 @@ require 'rails_helper'
 feature "routes acceptance spec" do
   scenario "user visits root path" do
     visit root_path
-    expect(page).to have_content("#{app_name}")
+    expect(page.status_code).to eql(200)
   end
 end
       TEST
